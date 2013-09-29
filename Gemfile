6150a3be9d8c4ddbb3f045a55c84ca2788690f4d
liyijie <liyijie825@gmail.com>
2013-09-29 17:14:52 +0800

    OBTAINING EMBER.JS AND DEPENDENCIES

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ end
 
 gem 'jquery-rails'
 
+# Ember.js
+gem 'ember-rails'
+gem 'ember-source'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,13 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require todos
+
+// for more details see: http://emberjs.com/guides/application/
+Todos = Ember.Application.create();
+
 //= require_tree .
